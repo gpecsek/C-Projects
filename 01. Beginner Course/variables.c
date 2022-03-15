ed5e015324def13b492ec545b8bc9ce5e657f42f
Gábor Pécsek <72249187+gpecsek@users.noreply.github.com>
2022-03-15 20:21:02 +0100

    Beginner course projects

---
+++ 01. Beginner Course/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("There once was a man called George\n");
+    printf("he was 70 years old.\n");
+    printf("He really liked the name George\n");
+    printf("but did not like being 70.\n");
+    return 0;
+}
